0dbead4d75edb94c1d5d7b4624d115f3247e57df
smltq <smltq@126.com>
2019-07-01 13:43:26 +0800

    介绍更新

--- shiro/src/main/java/demo/shiro/config/ShiroConfig.java
+++ shiro/src/main/java/demo/shiro/config/ShiroConfig.java
@@ -97,9 +97,11 @@ public class ShiroConfig {
     @Bean(name = "simpleMappingExceptionResolver")
     public SimpleMappingExceptionResolver createSimpleMappingExceptionResolver() {
         SimpleMappingExceptionResolver r = new SimpleMappingExceptionResolver();
+
         Properties mappings = new Properties();
         mappings.setProperty("DatabaseException", "databaseError"); //数据库异常处理
         mappings.setProperty("UnauthorizedException", "403");
+
         r.setExceptionMappings(mappings);  // None by default
         r.setDefaultErrorView("error");    // No default
         r.setExceptionAttribute("ex");     // Default is "exception"
