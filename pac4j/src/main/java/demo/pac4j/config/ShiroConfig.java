835e40879803e687c2d16a3da3cba357c082af9e
smltq <smltq@126.com>
2019-07-04 18:14:36 +0800

    认证中心-依赖更新

--- pac4j/src/main/java/demo/pac4j/config/ShiroConfig.java
+++ pac4j/src/main/java/demo/pac4j/config/ShiroConfig.java
@@ -1,115 +1,186 @@
 package demo.pac4j.config;
 
+import io.buji.pac4j.filter.CallbackFilter;
+import io.buji.pac4j.filter.LogoutFilter;
+import io.buji.pac4j.filter.SecurityFilter;
+import io.buji.pac4j.realm.Pac4jRealm;
+import io.buji.pac4j.subject.Pac4jSubjectFactory;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
-import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
-import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.shiro.mgt.SubjectFactory;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.pac4j.cas.client.CasClient;
+import org.pac4j.cas.client.rest.CasRestFormClient;
+import org.pac4j.cas.config.CasConfiguration;
+import org.pac4j.cas.config.CasProtocol;
+import org.pac4j.core.client.Clients;
+import org.pac4j.core.config.Config;
+import org.pac4j.http.client.direct.ParameterClient;
+import org.pac4j.jwt.config.encryption.SecretEncryptionConfiguration;
+import org.pac4j.jwt.config.signature.SecretSignatureConfiguration;
+import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator;
+import org.pac4j.jwt.profile.JwtGenerator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 
-import java.util.*;
+import javax.servlet.Filter;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Configuration
 @Slf4j
 public class ShiroConfig {
 
-    @Autowired
-    private IgnoreAuthUrlProperties ignoreAuthUrlProperties;
+    @Value("#{ @environment['cas.prefixUrl'] ?: null }")
+    private String prefixUrl;
+    @Value("#{ @environment['cas.loginUrl'] ?: null }")
+    private String casLoginUrl;
+    @Value("#{ @environment['cas.callbackUrl'] ?: null }")
+    private String callbackUrl;
+
+    //jwt秘钥
+    @Value("${jwt.salt}")
+    private String salt;
 
     @Bean
-    public ShiroFilterFactoryBean shirFilter(SecurityManager securityManager) {
-        log.info("Shiro过滤器开始处理");
-        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-        shiroFilterFactoryBean.setSecurityManager(securityManager);
-
-        // 配置登录页
-        shiroFilterFactoryBean.setLoginUrl("/login");
-        // 登录成功后跳转页面
-        shiroFilterFactoryBean.setSuccessUrl("/index");
-        //未授权界面
-        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
-
-        //拦截器
-        Map<String, String> filterMap = new LinkedHashMap<>();
-
-        //anon:所有url都都可以匿名访问
-        Set<String> urlSet = new HashSet<>(ignoreAuthUrlProperties.getIgnoreAuthUrl());
-        urlSet.stream().forEach(temp -> filterMap.put(temp, "anon"));
-
-        //配置退出 过滤器
-        filterMap.put("/logout", "logout");
-
-        //authc:所有url都必须认证通过才可以访问
-        filterMap.put("/**", "authc");
-
-        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterMap);
-        return shiroFilterFactoryBean;
+    public Realm pac4jRealm() {
+        return new Pac4jRealm();
     }
 
     /**
-     * 凭证匹配器
+     * cas核心过滤器，把支持的client写上，filter过滤时才会处理，clients必须在casConfig.clients已经注册
      *
      * @return
      */
     @Bean
-    public HashedCredentialsMatcher hashedCredentialsMatcher() {
-        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
-        //散列算法:这里使用MD5算法;
-        hashedCredentialsMatcher.setHashAlgorithmName("md5");
-        //散列的次数，比如散列两次，相当于 md5(md5(""));
-        hashedCredentialsMatcher.setHashIterations(2);
-        return hashedCredentialsMatcher;
-    }
-
-    @Bean
-    public AuthRealm authRealm() {
-        AuthRealm authRealm = new AuthRealm();
-        authRealm.setCredentialsMatcher(hashedCredentialsMatcher());
-        return authRealm;
+    public Filter casSecurityFilter() {
+        SecurityFilter filter = new SecurityFilter();
+        filter.setClients("CasClient,rest,jwt");
+        filter.setConfig(casConfig());
+        return filter;
     }
 
     /**
-     * 安全管理器
+     * JWT Token 生成器，对CommonProfile生成然后每次携带token访问
      *
      * @return
      */
     @Bean
-    public SecurityManager securityManager() {
-        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        securityManager.setRealm(authRealm());
-        return securityManager;
+    protected JwtGenerator jwtGenerator() {
+        return new JwtGenerator(new SecretSignatureConfiguration(salt), new SecretEncryptionConfiguration(salt));
     }
 
     /**
-     * 启用shiro注解
-     */
-    @Bean
-    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
-        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
-        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
-        return authorizationAttributeSourceAdvisor;
-    }
-
-    /**
-     * 异常处理
+     * 通过rest接口可以获取tgt，获取service ticket，甚至可以获取CasProfile
      *
      * @return
      */
-    @Bean(name = "simpleMappingExceptionResolver")
-    public SimpleMappingExceptionResolver createSimpleMappingExceptionResolver() {
-        SimpleMappingExceptionResolver r = new SimpleMappingExceptionResolver();
+    @Bean
+    protected CasRestFormClient casRestFormClient() {
+        CasRestFormClient casRestFormClient = new CasRestFormClient();
+        casRestFormClient.setConfiguration(casConfiguration());
+        casRestFormClient.setName("rest");
+        return casRestFormClient;
+    }
 
-        Properties mappings = new Properties();
-        mappings.setProperty("DatabaseException", "databaseError");
-        mappings.setProperty("UnauthorizedException", "403");
-        r.setExceptionMappings(mappings);
+    @Bean
+    protected Clients clients() {
+        //可以设置默认client
+        Clients clients = new Clients();
 
-        r.setDefaultErrorView("error");
-        r.setExceptionAttribute("ex");
-        return r;
+        //token校验器，可以用HeaderClient更安全
+        ParameterClient parameterClient = new ParameterClient("token", jwtAuthenticator());
+        parameterClient.setSupportGetRequest(true);
+        parameterClient.setName("jwt");
+        //支持的client全部设置进去
+        clients.setClients(casClient(), casRestFormClient(), parameterClient);
+        return clients;
+    }
+
+    /**
+     * JWT校验器，也就是目前设置的ParameterClient进行的校验器，是rest/或者前后端分离的核心校验器
+     *
+     * @return
+     */
+    @Bean
+    protected JwtAuthenticator jwtAuthenticator() {
+        JwtAuthenticator jwtAuthenticator = new JwtAuthenticator();
+        jwtAuthenticator.addSignatureConfiguration(new SecretSignatureConfiguration(salt));
+        jwtAuthenticator.addEncryptionConfiguration(new SecretEncryptionConfiguration(salt));
+        return jwtAuthenticator;
+    }
+
+    @Bean
+    protected Config casConfig() {
+        Config config = new Config();
+        config.setClients(clients());
+        return config;
+    }
+
+    /**
+     * cas的基本设置，包括或url等等，rest调用协议等
+     *
+     * @return
+     */
+    @Bean
+    public CasConfiguration casConfiguration() {
+        CasConfiguration casConfiguration = new CasConfiguration(casLoginUrl);
+        casConfiguration.setProtocol(CasProtocol.CAS30);
+        casConfiguration.setPrefixUrl(prefixUrl);
+        return casConfiguration;
+    }
+
+    @Bean
+    public CasClient casClient() {
+        CasClient casClient = new CasClient();
+        casClient.setConfiguration(casConfiguration());
+        casClient.setCallbackUrl(callbackUrl);
+        return casClient;
+    }
+
+    /**
+     * 路径过滤设置
+     *
+     * @return
+     */
+    @Bean
+    public ShiroFilterChainDefinition shiroFilterChainDefinition() {
+        DefaultShiroFilterChainDefinition definition = new DefaultShiroFilterChainDefinition();
+        definition.addPathDefinition("/callback", "callbackFilter");
+        definition.addPathDefinition("/logout", "logoutFilter");
+        definition.addPathDefinition("/**", "casSecurityFilter");
+        return definition;
+    }
+
+    /**
+     * 由于cas代理了用户，所以必须通过cas进行创建对象
+     *
+     * @return
+     */
+    @Bean
+    protected SubjectFactory subjectFactory() {
+        return new Pac4jSubjectFactory();
+    }
+
+    /**
+     * 对shiro的过滤策略进行明确
+     *
+     * @return
+     */
+    @Bean
+    protected Map<String, Filter> filters() {
+        //过滤器设置
+        Map<String, Filter> filters = new HashMap<>();
+        filters.put("casSecurityFilter", casSecurityFilter());
+        CallbackFilter callbackFilter = new CallbackFilter();
+        callbackFilter.setConfig(casConfig());
+        filters.put("callbackFilter", callbackFilter);
+        LogoutFilter logoutFilter = new LogoutFilter();
+        logoutFilter.setConfig(casConfig());
+        filters.put("logoutFilter", logoutFilter);
+        return filters;
     }
 }
