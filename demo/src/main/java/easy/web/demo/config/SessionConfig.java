3d9ba33a9e191f25dc5656120447896e3c75ecf4
smltq <smltq@126.com>
2019-07-15 22:25:27 +0800

    模块整理

--- demo/src/main/java/easy/web/demo/config/SessionConfig.java
+++
@@ -1,9 +0,0 @@
-package easy.web.demo.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400 * 30)
-public class SessionConfig {
-}
--- pac4j/service/src/main/java/demo/pac4j/sevice/UserInfoService.java
+++
@@ -1,8 +0,0 @@
-package demo.pac4j.sevice;
-
-import demo.pac4j.model.UserInfo;
-
-public interface UserInfoService {
-    /**通过username查找用户信息;*/
-    UserInfo findByUsername(String username);
-}
