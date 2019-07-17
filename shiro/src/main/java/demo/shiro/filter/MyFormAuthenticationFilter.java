7cef08f5b9cdad88321e28c623993e5b1cbf82c6
smltq <smltq@126.com>
2019-07-17 20:24:18 +0800

    未登录过滤器增加

---
+++ shiro/src/main/java/demo/shiro/filter/MyFormAuthenticationFilter.java
@@ -0,0 +1,70 @@
+package demo.shiro.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j
+public class MyFormAuthenticationFilter extends FormAuthenticationFilter {
+
+    /**
+     * 如果isAccessAllowed返回false 则执行onAccessDenied
+     *
+     * @param request
+     * @param response
+     * @param mappedValue
+     * @return
+     */
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+        if (request instanceof HttpServletRequest) {
+            if (((HttpServletRequest) request).getMethod().toUpperCase().equals("OPTIONS")) {
+                return true;
+            }
+        }
+        return super.isAccessAllowed(request, response, mappedValue);
+    }
+
+    /**
+     * 在访问controller前判断是否登录，返回json，不进行重定向。
+     *
+     * @param request
+     * @param response
+     * @return true-继续往下执行，false-该filter过滤器已经处理，不继续执行其他过滤器
+     * @throws Exception
+     */
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws IOException {
+        log.info("未登录禁止访问");
+        saveRequest(request);
+        setHeader((HttpServletRequest) request, (HttpServletResponse) response);
+
+        PrintWriter out = response.getWriter();
+        ((HttpServletResponse) response).setStatus(401);
+        String result = "未登录";
+        out.println(result);
+        out.flush();
+        out.close();
+        return false;
+    }
+
+    /**
+     * 为response设置header，实现跨域
+     */
+    private void setHeader(HttpServletRequest request, HttpServletResponse response) {
+        //跨域的header设置
+        response.setHeader("Access-control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Methods", request.getMethod());
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
+        //防止乱码，适用于传输JSON数据
+        response.setHeader("Content-Type", "application/json;charset=UTF-8");
+        response.setStatus(200);
+    }
+}
