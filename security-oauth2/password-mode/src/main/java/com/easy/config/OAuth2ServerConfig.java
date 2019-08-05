232a709128f2ce9a3a1c0023db9c72bde75de31f
smltq <smltq@126.com>
2019-08-05 18:44:29 +0800

    oauth2 密码模式示例

--- security-oauth2/password-mode/src/main/java/com/easy/config/OAuth2ServerConfig.java
+++ security-oauth2/password-mode/src/main/java/com/easy/config/OAuth2ServerConfig.java
@@ -33,8 +33,8 @@ public class OAuth2ServerConfig {
         }
 
         @Override
-        public void configure(HttpSecurity http) throws Exception {
-            http
+        public void configure(HttpSecurity httpSecurity) throws Exception {
+            httpSecurity
                     .authorizeRequests()
                     .antMatchers("/order/**").authenticated();//配置order访问控制，必须认证过后才可以访问
 
