9128c952d6299c3939252b3605939980244704dc
smltq <smltq@126.com>
2019-08-05 17:33:55 +0800

    security-oauth2示例

--- security-oauth2/HELP.md
+++ security-oauth2/HELP.md
@@ -1,1 +1,6 @@
 # Spring Boot SecurityOauth2
+
+## 资料
+
+[官方文档](https://spring.io/guides/tutorials/spring-boot-oauth2/)
+[OAuth2 Boot](https://docs.spring.io/spring-security-oauth2-boot/docs/current-SNAPSHOT/reference/htmlsingle/)
---
+++ security-oauth2/src/main/java/com/easy/web/TestEndpoints.java
@@ -0,0 +1,25 @@
+package com.easy.web;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestEndpoints {
+
+    @GetMapping("/product/{id}")
+    public String getProduct(@PathVariable String id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "product id : " + id;
+    }
+
+    @GetMapping("/order/{id}")
+    public String getOrder(@PathVariable String id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "order id : " + id;
+    }
+
+
+}
