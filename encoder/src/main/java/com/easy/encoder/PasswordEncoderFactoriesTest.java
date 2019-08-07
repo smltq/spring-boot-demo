ac8391d0b5e10716eacc0f0ec7151e5584bc329a
smltq <smltq@126.com>
2019-08-07 17:22:49 +0800

    密码相关

--- encoder/src/main/java/com/easy/encoder/PasswordEncoderFactoriesTest.java
+++ encoder/src/main/java/com/easy/encoder/PasswordEncoderFactoriesTest.java
@@ -1,30 +1,64 @@
 package com.easy.encoder;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @Slf4j
 public class PasswordEncoderFactoriesTest {
     public static void main(String[] args) {
         String password = "123456";
-        PasswordEncoder defaultEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
         log.info("PasswordEncoderFactories默认加密算法是bcrypt");
-        String encodeStr1 = defaultEncoder.encode(password);
-        String encodeStr2 = defaultEncoder.encode(password);
-        log.info("明文==>{},密文==>{},是否匹配==>{}", password, encodeStr1, defaultEncoder.matches(password, encodeStr1));
-        log.info("明文==>{},密文==>{},是否匹配==>{}", password, encodeStr2, defaultEncoder.matches(password, encodeStr2));
+        String encodeStr1 = passwordEncoder.encode(password);
+        String encodeStr2 = passwordEncoder.encode(password);
+        log.info("明文==>{},密文==>{},是否匹配==>{}", password, encodeStr1, passwordEncoder.matches(password, encodeStr1));
+        log.info("明文==>{},密文==>{},是否匹配==>{}", password, encodeStr2, passwordEncoder.matches(password, encodeStr2));
 
-        // 测试2:工厂产生的 PasswordEncoder 会根据密码密文encoderId前缀对应的PasswordEncoder进行密码匹配
-        System.out.println("测试2:工厂产生的 PasswordEncoder 会根据密码密文encoderId前缀对应的PasswordEncoder进行密码匹配");
-        final String passwordPlainText = "password";
-        String cypher1 = "{noop}password";
-        String cypher2 = "{pbkdf2}5d923b44a6d129f3ddf3e3c8d29412723dcbde72445e8ef6bf3b508fbf17fa4ed4d6b99ca763d8dc";
-        String cypher4 = "{sha256}97cde38028ad898ebc02e690819fa220e88c62e0699403e94fff291cfffaf8410849f27605abcbc0";
+        String[] encodes = {"bcrypt", "ldap", "MD4", "MD5", "noop", "pbkdf2", "scrypt", "SHA-1", "SHA-256", "sha256"};
 
-        log.info("密码密文1和密码明文匹配 :{}", defaultEncoder.matches(passwordPlainText, cypher1));
-        log.info("密码密文2和密码明文匹配 :{}", defaultEncoder.matches(passwordPlainText, cypher2));
-        log.info("密码密文4和密码明文匹配 :{}", defaultEncoder.matches(passwordPlainText, cypher4));
+        for (String encode : encodes) {
+            passwordEncoder = newPasswordEncoder(encode);
+            String encodeStr = passwordEncoder.encode(password);
+            log.info("{}算法,明文==>{},密文==>{}", encode, password, encodeStr);
+        }
+
+//        log.info("密码密文1和密码明文匹配 :{}", defaultEncoder.matches(passwordPlainText, cypher1));
+//        log.info("密码密文2和密码明文匹配 :{}", defaultEncoder.matches(passwordPlainText, cypher2));
+//        log.info("密码密文4和密码明文匹配 :{}", defaultEncoder.matches(passwordPlainText, cypher3));
+    }
+
+
+    public static PasswordEncoder newPasswordEncoder(final String encoderType) {
+
+        switch (encoderType) {
+            case "bcrypt":
+                return new BCryptPasswordEncoder();
+            case "ldap":
+                return new org.springframework.security.crypto.password.LdapShaPasswordEncoder();
+            case "MD4":
+                return new org.springframework.security.crypto.password.Md4PasswordEncoder();
+            case "MD5":
+                return new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("MD5");
+            case "noop":
+                return org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance();
+            case "pbkdf2":
+                return new Pbkdf2PasswordEncoder();
+            case "scrypt":
+                return new SCryptPasswordEncoder();
+            case "SHA-1":
+                return new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-1");
+            case "SHA-256":
+                return new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-256");
+            case "sha256":
+                return new org.springframework.security.crypto.password.StandardPasswordEncoder();
+            default:
+                return NoOpPasswordEncoder.getInstance();
+        }
     }
 }
