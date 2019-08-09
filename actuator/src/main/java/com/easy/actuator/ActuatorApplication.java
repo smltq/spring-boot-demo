5782d5fa98ecad191e1d1b7431648e9341d1bcf0
smltq <smltq@126.com>
2019-08-09 10:53:36 +0800

    Actuator示例

---
+++ actuator/src/main/java/com/easy/actuator/ActuatorApplication.java
@@ -0,0 +1,11 @@
+package com.easy.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+}
