e2d5258ad779cc716557dac3adc693e7e27c7e33
smltq <smltq@126.com>
2019-08-10 14:30:29 +0800

    admin示例

--- admin/admin-server/src/main/java/com/easy/AdminServerApplication.java
+++ admin/admin-server/src/main/java/com/easy/AdminServerApplication.java
@@ -2,11 +2,14 @@ package com.easy;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableAdminServer
 public class AdminServerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AdminServerApplication.class, args);
     }
