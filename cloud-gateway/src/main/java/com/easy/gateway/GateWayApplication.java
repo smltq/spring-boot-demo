be721263557b5a2e2dda4d2da70c09cc7fcfb516
lintq <smltq@126.com>
2019-08-19 15:02:06 +0800

    cloud gateway网关示例

--- cloud-gateway/src/main/java/com/easy/gateway/GateWayApplication.java
+++ cloud-gateway/src/main/java/com/easy/gateway/GateWayApplication.java
@@ -3,18 +3,10 @@ package com.easy.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class GateWayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
     }
-
-//    @Bean
-//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("path_route", r -> r.path("/about").uri("https://www.cnblogs.com/tqlin/"))
-//                .build();
-//    }
 }
