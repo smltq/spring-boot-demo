7e001274a577d57fa062a58b449b6dc07cbaa076
lintq <smltq@126.com>
2019-08-20 19:58:23 +0800

    cloud-config示例

---
+++ cloud-config/cloud-config-client/src/test/java/com/easy/cloudConfigClient/ApplicationTests.java
@@ -0,0 +1,22 @@
+package java.com.easy.cloudConfigClient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+    @Value("${neo.hello}")
+    private String hello;
+
+    @Test
+    public void contextLoads() {
+        System.out.println("************************************************************");
+        System.out.println("hello value : " + hello);
+        System.out.println("************************************************************");
+    }
+}
