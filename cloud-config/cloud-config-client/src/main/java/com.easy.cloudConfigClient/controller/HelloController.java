46014d0635c23b3802a829a4b3afd20c1d85f34d
lintq <smltq@126.com>
2019-08-21 18:54:29 +0800

    cloud-config示例

--- cloud-config/cloud-config-client/src/main/java/com.easy.cloudConfigClient/controller/HelloController.java
+++ cloud-config/cloud-config-client/src/main/java/com.easy.cloudConfigClient/controller/HelloController.java
@@ -1,16 +1,19 @@
 package com.easy.cloudConfigClient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
-public class TestController {
+public class HelloController {
     @Value("${easy.hello}")
     private String hello;
 
     @RequestMapping("/hello")
     public String hello() {
-        return this.hello;
+        return hello;
     }
 }
