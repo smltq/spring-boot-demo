6690976b0c41abbfbfddda3664fa081244ea4bcb
lintq <smltq@126.com>
2019-08-29 14:53:56 +0800

    mybatis数据库集群（读写分离）示例

--- mybatis-multi-datasource/src/test/java/com/easy/mybatis/multidatasource/MultiDataSourceServiceTest.java
+++ mybatis-multi-datasource/src/test/java/com/easy/mybatis/multidatasource/MultiDataSourceServiceTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
