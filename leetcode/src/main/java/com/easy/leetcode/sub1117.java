06930575e2ca4af0d82b3290527eaba289ade982
lintq <smltq@126.com>
2019-08-30 17:25:52 +0800

    力扣1117题

---
+++ leetcode/src/main/java/com/easy/leetcode/sub1117.java
@@ -0,0 +1,27 @@
+package com.easy.leetcode;
+
+public class sub1117 {
+    public static void main(String[] args) {
+        H2O h2o = new H2O();
+        System.out.println("sdfdsf");
+    }
+}
+
+class H2O {
+
+    public H2O() {
+
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+
+        // releaseOxygen.run() outputs "H". Do not change or remove this line.
+        releaseOxygen.run();
+    }
+}
