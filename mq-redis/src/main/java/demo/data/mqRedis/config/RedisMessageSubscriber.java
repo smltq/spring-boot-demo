abacb1b7a52fa87d72769be666321de29ab6b836
lintq <smltq@126.com>
2019-09-05 15:51:57 +0800

    mq-redis示例

--- mq-redis/src/main/java/demo/data/mqRedis/config/RedisMessageSubscriber.java
+++ mq-redis/src/main/java/demo/data/mqRedis/config/RedisMessageSubscriber.java
@@ -1,5 +1,6 @@
 package demo.data.mqRedis.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,13 @@ import java.util.List;
  * 消息订阅方
  */
 @Service
+@Slf4j
 public class RedisMessageSubscriber implements MessageListener {
 
     public static List<String> messageList = new ArrayList<>();
 
     public void onMessage(Message message, byte[] pattern) {
         messageList.add(message.toString());
-        System.out.println("Message received: " + message.toString());
+        log.info("订阅方接收到了消息==>{}", message.toString());
     }
 }
