33207fa8795ffb5e0a8967ca8c229289d3859067
lintq <smltq@126.com>
2019-09-06 16:16:03 +0800

    ThreadLocal源码解析

--- cloud-config/cloud-config-client/src/main/java/com.easy.cloudConfigClient/controller/HelloController.java
+++ cloud-config/cloud-config-client/src/main/java/com.easy.cloudConfigClient/controller/HelloController.java
@@ -1,11 +1,8 @@
 package com.easy.cloudConfigClient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Locale;
 
 @RestController
 public class HelloController {
---
+++ java-gather/src/main/java/com/easy/javaGather/ThreadLocal/ThreadLocal.md
@@ -0,0 +1,9 @@
+# Java并发编程之ThreadLocal解析
+
+![](ThreadLocal.png)
+
+本文讨论的是JDK 1.8中的ThreadLocal
+
+## ThreadLocal的常用方法
+
+### add
