3441ebdbeda982c1c6ff69a038ad4c93f0a4a3b1
lintq <smltq@126.com>
2019-09-16 14:04:30 +0800

    Spring Cloud Feign 声明式服务调用示例

--- cloud-feign/hello-service-api/src/main/java/com/easy/helloServiceApi/client/GoodsServiceClient.java
+++ cloud-feign/hello-service-api/src/main/java/com/easy/helloServiceApi/client/GoodsServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(value = "GOODS")
+@FeignClient(value = "hello-server")
 public interface GoodsServiceClient {
 
     @RequestMapping("/goods/goodsInfo/{goodsId}")
