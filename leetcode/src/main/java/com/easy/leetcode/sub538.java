d37c5109cfc7d13a652ce8a08e6fb48e5a482d17
ltq <smltq@126.com>
2019-09-21 18:13:53 +0800

    leetcode538題

--- leetcode/src/main/java/com/easy/leetcode/sub538.java
+++ leetcode/src/main/java/com/easy/leetcode/sub538.java
@@ -1,18 +1,49 @@
 package com.easy.leetcode;
 
 public class sub538 {
-    int sum=0;
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(13);
+
+        Solution solution=new Solution();
+        solution.convertBST(root);
+        outTree(root);
+    }
+
+    static void outTree(TreeNode root){
+        if(root!=null) {
+            System.out.print(root.val+"\t");
+            outTree(root.left);
+            outTree(root.right);
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    int sum = 0;
+
     public TreeNode convertBST(TreeNode root) {
-        sum=0;
+        sum = 0;
         addSum(root);
         return root;
     }
 
-    public void addSum(TreeNode root){
-        if(root!=null){
+    public void addSum(TreeNode root) {
+        if (root != null) {
             addSum(root.right);
-            sum+=root.val;
-            root.val=sum;
+            sum += root.val;
+            root.val = sum;
             addSum(root.left);
         }
     }
