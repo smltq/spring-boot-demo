273672fc7ea9ad1592f189318b029835f5871979
ltq <smltq@126.com>
2019-10-04 07:42:09 +0800

    力扣1117题

--- leetcode/src/main/java/com/easy/leetcode/Sub1117.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub1117.java
@@ -3,16 +3,16 @@ package com.easy.leetcode;
 public class Sub1117 {
     public static void main(String[] args) throws InterruptedException {
         String test = "HOHOHHOOHOHHHHHOHHHOH";
-        System.out.println("当前数组长度：" + test.length());
+        //System.out.println("当前数组长度：" + test.length());
         H2O_1 h2o = new H2O_1();
         for (int i = 0; i < test.length(); ++i) {
             //System.out.println("当前索引值：" + i);
             if (test.charAt(i) == 'O') {
-                //h2o.oxygen(new OThread());
-                new H2O_1().oxygen(new OThread());
+                h2o.oxygen(new OThread());
+                //new H2O_1().oxygen(new OThread());
             } else if (test.charAt(i) == 'H') {
-                //h2o.hydrogen(new HThread());
-                new H2O_1().hydrogen(new HThread());
+                h2o.hydrogen(new HThread());
+                //new H2O_1().hydrogen(new HThread());
             }
         }
     }
