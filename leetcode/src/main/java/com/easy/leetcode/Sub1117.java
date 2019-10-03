aa2f38a40cd46582594109207dba75728915caa7
ltq <smltq@126.com>
2019-10-03 21:00:32 +0800

    力扣1117题

--- leetcode/src/main/java/com/easy/leetcode/Sub1117.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub1117.java
@@ -1,98 +1,109 @@
 package com.easy.leetcode;
 
 import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Sub1117 {
     public static void main(String[] args) throws InterruptedException {
         String test = "HOHOHHOOHOHHHHHOHHHOH";
-        H2O h2o = new H2O();
+        System.out.println("当前数组长度：" + test.length());
         for (int i = 0; i < test.length(); ++i) {
+            //System.out.println("当前索引值：" + i);
             if (test.charAt(i) == 'O') {
-                h2o.oxygen(new Runnable() {
-                    public void run() {
-                        //System.out.print(test.charAt(i));
-                    }
-                });
+                new H2O_2().oxygen(new OThread());
             } else if (test.charAt(i) == 'H') {
-
+                new H2O_2().hydrogen(new HThread());
             }
         }
     }
 }
 
-class H2O {
-    private int wH, wO;
-    private Lock lock;
-    private Condition waitingH, waitingO, barrier;
-    private int aH, aO;
+//生产氢气
+class HThread implements Runnable {
+    public void run() {
+        System.out.print("H");
+    }
+}
 
-    public H2O() {
-        wH = 0;
-        wO = 0;
-        aH = 0;
-        aO = 0;
-        lock = new ReentrantLock();
-        waitingH = lock.newCondition();
-        waitingO = lock.newCondition();
-        barrier = lock.newCondition();
+//生产氧气
+class OThread implements Runnable {
+    public void run() {
+        System.out.print("O");
+    }
+}
+
+class H2O_2 {
+    private int flagH = 0;
+    private int flagO = 0;
+    private Object lock = new Object();
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        synchronized (lock) {
+            if (flagO * 2 >= flagH) {
+                flagH++;
+                releaseHydrogen.run();
+            } else {
+                releaseHydrogen.wait();
+            }
+        }
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        synchronized (lock) {
+            if (flagO == 0 || flagO * 2 <= flagH) {
+                flagO++;
+                releaseOxygen.run();
+            } else {
+                releaseOxygen.wait();
+            }
+        }
+    }
+}
+
+class H2O_1 {
+    private int flagH = 0;
+    private int flagO = 0;
+    private ReentrantLock lock;
+    private Condition condition;
+
+    public H2O_1() {
+        lock = new ReentrantLock(true);
+        condition = lock.newCondition();
     }
 
     public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
-        lock.lock();
-        try {
-            wH++;
-            // while not allowed to leave
-            while (aH == 0) {
-                if (wH >= 2 && wO >= 1) {
-                    if (aO > 0) {
-                        barrier.await();
-                    }
-                    wH -= 2;
-                    aH += 2;
-                    wO -= 1;
-                    aO += 1;
-                    waitingH.signal();
-                    waitingO.signal();
-                } else {
-                    waitingH.await();
-                    barrier.signal();
-                }
+        while (true) {
+            lock.lock();
+            boolean b = flagH < 2;
+            if (b) {
+                releaseHydrogen.run();
+                flagH++;
+                condition.signalAll();
+                lock.unlock();
+                break;
+            } else {
+                condition.signalAll();
+                condition.await();
             }
-            aH--;
-        } finally {
-            releaseHydrogen.run();
             lock.unlock();
         }
     }
 
     public void oxygen(Runnable releaseOxygen) throws InterruptedException {
-        lock.lock();
-        try {
-            wO++;
-
-            while (aO == 0) {
-                if (wH >= 2 && wO >= 1) {
-                    if (aH > 0) {
-                        barrier.await();
-                    }
-                    wH -= 2;
-                    aH += 2;
-                    wO -= 1;
-                    aO += 1;
-                    waitingH.signal();
-                    waitingH.signal();
-                } else {
-                    waitingO.await();
-                    barrier.signal();
-                }
+        while (true) {
+            lock.lock();
+            boolean b = flagO == 0 || flagO * 2 <= flagH;
+            if (b) {
+                releaseOxygen.run();
+                flagO++;
+                condition.signalAll();
+                lock.unlock();
+                break;
+            } else {
+                condition.signalAll();
+                condition.await();
             }
-            aO--;
-        } finally {
-            releaseOxygen.run();
             lock.unlock();
         }
-
     }
 }
