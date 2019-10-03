8ab033ce245153a5f663faf0def1a24148cad6d6
ltq <smltq@126.com>
2019-10-03 22:00:56 +0800

    力扣1117题

--- leetcode/src/main/java/com/easy/leetcode/Sub1117.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub1117.java
@@ -1,17 +1,17 @@
 package com.easy.leetcode;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class Sub1117 {
     public static void main(String[] args) throws InterruptedException {
         String test = "HOHOHHOOHOHHHHHOHHHOH";
         System.out.println("当前数组长度：" + test.length());
+        H2O_1 h2o = new H2O_1();
         for (int i = 0; i < test.length(); ++i) {
             //System.out.println("当前索引值：" + i);
             if (test.charAt(i) == 'O') {
+                //h2o.oxygen(new OThread());
                 new H2O_1().oxygen(new OThread());
             } else if (test.charAt(i) == 'H') {
+                //h2o.hydrogen(new HThread());
                 new H2O_1().hydrogen(new HThread());
             }
         }
@@ -33,9 +33,8 @@ class OThread implements Runnable {
 }
 
 class H2O_1 {
-
+    public H2O_1() {}
     int h = 0;
-
     public synchronized void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
         while (h == 2) {
             wait();
@@ -47,6 +46,7 @@ class H2O_1 {
 
     public synchronized void oxygen(Runnable releaseOxygen) throws InterruptedException {
         while (h < 2) {
+            System.out.println("\t氢气\t" + h);
             wait();
         }
         releaseOxygen.run();
