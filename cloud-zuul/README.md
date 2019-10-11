0410694fc9ebfb8963b56c1669486f7ad93cd856
lintq <smltq@126.com>
2019-10-11 19:56:49 +0800

    zuul示例

---
+++ cloud-zuul/README.md
@@ -0,0 +1,7 @@
+# Spring Cloud Zuul
+
+## 开发环境
+
+- jdk 1.8
+- Spring Boot 2.1.9
+- Spring Cloud Greenwich SR3
---
+++ cloud-zuul/zuul-eureka-server/src/main/java/com/easy/eurekaServer/ZuulEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.easy.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ZuulEurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulEurekaServerApplication.class, args);
+    }
+}
