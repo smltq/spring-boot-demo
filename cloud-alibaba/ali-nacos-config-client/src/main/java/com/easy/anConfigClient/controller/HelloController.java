fbdd24e373b13cdb97582f1c6d80396cf052a9fe
lintq <smltq@126.com>
2019-10-22 17:00:41 +0800

    config示例

--- cloud-alibaba/ali-nacos-config-client/src/main/java/com/easy/anConfigClient/controller/HelloController.java
+++ cloud-alibaba/ali-nacos-config-client/src/main/java/com/easy/anConfigClient/controller/HelloController.java
@@ -4,13 +4,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
     @Value("${easy.hello}")
     private String hello;
 
+    @Value("${easy.myconfig}")
+    private String myconfig;
+
     @RequestMapping("/hello")
-    public String hello() {
-        return hello;
+    public Map hello() {
+        Map map = new HashMap<>();
+        map.put("hello", hello);
+        map.put("myconfig", myconfig);
+        return map;
     }
 }
