3fb061a153b7e7ecd69426be6cf47453abe7894e
lintq <smltq@126.com>
2019-11-05 13:48:53 +0800

    力扣103题

--- leetcode/src/main/java/com/easy/leetcode/Sub103.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub103.java
@@ -1,9 +1,6 @@
 package com.easy.leetcode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /*
 给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
@@ -51,33 +48,34 @@ class Solution103_1 {
     }
 
     List<List<Integer>> BFS(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+        Deque<TreeNode> deque = new LinkedList<>();
+        Deque<TreeNode> tDeque = new LinkedList<>();
+        deque.push(root);
         List<List<Integer>> result = new LinkedList<>();
         boolean reverse = true;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
+        while (!deque.isEmpty()) {
+            int size = deque.size();
             List<Integer> subResult = new LinkedList<>();
             for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                subResult.add(node.val);
+                TreeNode node = null;
                 if (reverse) {
-                    if (node.right != null) {
-                        queue.add(node.right);
-                    }
-                    if (node.left != null) {
-                        queue.add(node.left);
-                    }
+                    node = deque.removeFirst();
                 } else {
-                    if (node.left != null) {
-                        queue.add(node.left);
-                    }
-                    if (node.right != null) {
-                        queue.add(node.right);
-                    }
+                    node = deque.removeLast();
                 }
+
+                if (node.left != null) {
+                    tDeque.add(node.left);
+                }
+                if (node.right != null) {
+                    tDeque.add(node.right);
+                }
+
+                subResult.add(node.val);
             }
             result.add(subResult);
+            deque = tDeque;
+            tDeque = new LinkedList<>();
             //System.out.println(reverse);
             reverse = !reverse;
         }
