3cd78711edd5a3b4e0668c51301ebe378697efb5
lintq <smltq@126.com>
2019-11-05 18:58:29 +0800

    力扣103题

--- leetcode/src/main/java/com/easy/leetcode/Sub103.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub103.java
@@ -1,6 +1,9 @@
 package com.easy.leetcode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
 
 /*
 给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
@@ -26,17 +29,17 @@ import java.util.*;
  */
 public class Sub103 {
     public static void main(String[] args) {
-//        TreeNode root = new TreeNode(3);
-//        root.left = new TreeNode(9);
-//        root.right = new TreeNode(20);
-//        root.right.left = new TreeNode(15);
-//        root.right.right = new TreeNode(7);
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
 
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        root.left.left = new TreeNode(4);
-        root.right.right = new TreeNode(5);
+//        TreeNode root = new TreeNode(1);
+//        root.left = new TreeNode(2);
+//        root.right = new TreeNode(3);
+//        root.left.left = new TreeNode(4);
+//        root.right.right = new TreeNode(5);
 
         Solution103_1 solution = new Solution103_1();
         List<List<Integer>> list = solution.zigzagLevelOrder(root);
@@ -57,7 +60,7 @@ class Solution103_1 {
         Deque<TreeNode> deque = new LinkedList<>();
         deque.addLast(root);
         List<List<Integer>> result = new LinkedList<>();
-        boolean reverse = false;
+        boolean reverse = true;
         while (!deque.isEmpty()) {
             int size = deque.size();
             List<Integer> subResult = new LinkedList<>();
@@ -65,17 +68,20 @@ class Solution103_1 {
                 TreeNode node;
                 if (reverse) {
                     node = deque.pollFirst();
+                    if (node.left != null) {
+                        deque.addLast(node.left);
+                    }
+                    if (node.right != null) {
+                        deque.addLast(node.right);
+                    }
                 } else {
                     node = deque.pollLast();
-                }
-                if (node == null) {
-                    continue;
-                }
-                if (node.right != null) {
-                    deque.addLast(node.right);
-                }
-                if (node.left != null) {
-                    deque.addLast(node.left);
+                    if (node.right != null) {
+                        deque.addFirst(node.right);
+                    }
+                    if (node.left != null) {
+                        deque.addFirst(node.left);
+                    }
                 }
                 subResult.add(node.val);
             }
