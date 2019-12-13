54e0da23b9bf316957b537797b4491629e79487b
lintq <smltq@126.com>
2019-12-13 18:14:33 +0800

    java nio 介绍

--- netty/README.md
+++ netty/README.md
@@ -1,6 +1,6 @@
 # Spring Boot 搭建TCP Server
 
-本示例实现BIO通信、NIO通信及利用Netty实现NIO通信。
+本示例利用Java原生API实现BIO通信、NIO通信及利用Netty实现NIO通信。
 
 Netty 是一个异步事件驱动的网络应用程序框架，用于快速开发可维护的高性能协议服务器和客户端。
 
---
+++ netty/netty-server/src/main/java/com/easy/nettyServer/NettyServerApplication.java
@@ -0,0 +1,12 @@
+package com.easy.nettyServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyServerApplication.class, args);
+    }
+}
