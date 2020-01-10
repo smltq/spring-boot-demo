43ea333c5b7e05341535010d767b54e8410c31c3
lintq <smltq@126.com>
2020-01-10 12:20:28 +0800

    leetcode第49题

--- leetcode/src/main/java/com/easy/leetcode/Sub49.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub49.java
@@ -44,7 +44,9 @@ class Solution_49 {
         int[] count = new int[26];
         for (String s : strs) {
             Arrays.fill(count, 0);
-            for (char c : s.toCharArray()) count[c - 'a']++;
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
 
             StringBuilder sb = new StringBuilder("");
             for (int i = 0; i < 26; i++) {
