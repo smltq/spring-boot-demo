e11c87981265e2fc2e48af089f4effec57d59868
lintq <smltq@126.com>
2020-01-10 14:23:02 +0800

    leetcode第49题

--- leetcode/src/main/java/com/easy/leetcode/Sub49.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub49.java
@@ -69,6 +69,10 @@ class Solution_49_1 {
     }
 }
 
+/**
+ * 算术基本定理，又称为正整数的唯一分解定理，即：每个大于1的自然数，要么本身就是质数，要么可以写为2个或以上的质数的积，而且这些质因子按大小排列之后，写法仅有一种方式
+ * [维基百科](https://zh.wikipedia.org/wiki/%E7%AE%97%E6%9C%AF%E5%9F%BA%E6%9C%AC%E5%AE%9A%E7%90%86)
+ */
 class Solution_49_2 {
     public List<List<String>> groupAnagrams(String[] strs) {
         int[] table = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
