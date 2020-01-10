8a1804d13b2c2944c058fbe77a01917218b5b76e
lintq <smltq@126.com>
2020-01-10 12:19:16 +0800

    leetcode第49题

--- leetcode/src/main/java/com/easy/leetcode/Sub49.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub49.java
@@ -39,23 +39,22 @@ public class Sub49 {
 
 class Solution_49 {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        for (String str : strs) {
-            Map<Character, Integer> charMap = convertMap(str);
-        }
-        return result;
-    }
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
 
-    public Map convertMap(String str) {
-        Map<Character, Integer> result = new HashMap();
-        for (char ch : str.toCharArray()) {
-            if (result.containsKey(ch)) {
-                int val = result.get(ch);
-                result.put(ch, val++);
-            } else {
-                result.put(ch, 1);
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
             }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
         }
-        return result;
+        return new ArrayList(ans.values());
     }
 }
