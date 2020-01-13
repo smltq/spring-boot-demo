194a4a3a3ad4b6c10d1bf520d39fa1cf4b648565
lintq <smltq@126.com>
2020-01-13 11:37:24 +0800

    leetcode第4题

--- leetcode/src/main/java/com/easy/leetcode/Sub4.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub4.java
@@ -1,10 +1,33 @@
 package com.easy.leetcode;
 
+/*
+给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+
+请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+
+你可以假设 nums1 和 nums2 不会同时为空。
+
+示例 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+则中位数是 2.0
+示例 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+则中位数是 (2 + 3)/2 = 2.5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
+*/
 public class Sub4 {
     public static void main(String[] args) {
         Solution_4 solution = new Solution_4();
-        int[] nums1 = new int[]{1, 2};
-        int[] nums2 = new int[]{3, 4};
+        int[] nums1 = new int[]{1, 3, 4, 9};
+        int[] nums2 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         double median = solution.findMedianSortedArrays(nums1, nums2);
         System.out.println("中位数是：" + median);
     }
@@ -14,23 +37,18 @@ class Solution_4 {
     public double findMedianSortedArrays(int[] A, int[] B) {
         int m = A.length;
         int n = B.length;
-        if (m > n) { // 这里保证m<=n
-            int[] temp = A;
-            A = B;
-            B = temp;
-            int tmp = m;
-            m = n;
-            n = tmp;
+        if (m > n) {
+            return findMedianSortedArrays(B, A); // 保证 m <= n
         }
-        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        int iMin = 0, iMax = m;
         while (iMin <= iMax) {
             int i = (iMin + iMax) / 2;
-            int j = halfLen - i;
-            if (i < iMax && B[j - 1] > A[i]) {
-                iMin = i + 1; // i is too small
-            } else if (i > iMin && A[i - 1] > B[j]) {
-                iMax = i - 1; // i is too big
-            } else { // i is perfect
+            int j = (m + n + 1) / 2 - i;
+            if (j != 0 && i != m && B[j - 1] > A[i]) { // i 需要增大
+                iMin = i + 1;
+            } else if (i != 0 && j != n && A[i - 1] > B[j]) { // i 需要减小
+                iMax = i - 1;
+            } else { // 达到要求，并且将边界条件列出来单独考虑
                 int maxLeft = 0;
                 if (i == 0) {
                     maxLeft = B[j - 1];
@@ -41,7 +59,7 @@ class Solution_4 {
                 }
                 if ((m + n) % 2 == 1) {
                     return maxLeft;
-                }
+                } // 奇数的话不需要考虑右半部分
 
                 int minRight = 0;
                 if (i == m) {
@@ -52,7 +70,7 @@ class Solution_4 {
                     minRight = Math.min(B[j], A[i]);
                 }
 
-                return (maxLeft + minRight) / 2.0;
+                return (maxLeft + minRight) / 2.0; //如果是偶数的话返回结果
             }
         }
         return 0.0;
