043e022f63203332ad1b20fd8139423fedb10d90
lintq <smltq@126.com>
2020-02-14 14:05:32 +0800

    leetcode第38题

--- leetcode/src/main/java/com/easy/leetcode/Sub38.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub38.java
@@ -35,18 +35,38 @@ package com.easy.leetcode;
 public class Sub38 {
     public static void main(String[] args) {
         Solution_38 solution = new Solution_38();
-        int n = 1;
+        int n = 4;
         String result = solution.countAndSay(n);
         System.out.println("返回结果为：" + result);
     }
 }
 
+/**
+ * 解题思路：
+ * 本题的难点在于：报数的概念理解
+ * 从4->5分析，将4个每一位拆开看（个数+数字），4=1211 => 1=11，2=12，11=21，所以5=111221
+ * 所以解题用循环，从1->n可求解出来
+ */
 class Solution_38 {
     public String countAndSay(int n) {
-        StringBuffer result = new StringBuffer(100);
-        for (int i = 0; i < n; i++) {
-
+        String str = "1";
+        for (int i = 2; i <= n; i++) {
+            StringBuilder builder = new StringBuilder();
+            char pre = str.charAt(0);
+            int count = 1;
+            for (int j = 1; j < str.length(); j++) {
+                char c = str.charAt(j);
+                if (c == pre) {
+                    count++;
+                } else {
+                    builder.append(count).append(pre);
+                    pre = c;
+                    count = 1;
+                }
+            }
+            builder.append(count).append(pre);
+            str = builder.toString();
         }
-        return result.toString();
+        return str;
     }
 }
