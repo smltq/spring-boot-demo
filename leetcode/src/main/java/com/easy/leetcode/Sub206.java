5661d8967cf5637c44fdf6d0d8013b3a309b45d4
lintq <smltq@126.com>
2020-02-25 18:03:59 +0800

    leetcode 第 206 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub206.java
@@ -0,0 +1,55 @@
+package com.easy.leetcode;
+
+import java.util.Stack;
+
+/*
+
+206. 反转链表
+
+反转一个单链表。
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+
+ */
+public class Sub206 {
+    public static void main(String[] args) {
+        Solution_206 solution = new Solution_206();
+        ListNode root = new ListNode(1);
+
+        root.next = new ListNode(2);
+        root.next.next = new ListNode(3);
+        root.next.next.next = new ListNode(4);
+        root.next.next.next.next = new ListNode(5);
+
+        ListNode result = solution.reverseList(root);
+        while (result != null) {
+            System.out.print("-->" + result.val);
+            result = result.next;
+        }
+    }
+}
+
+class Solution_206 {
+    public ListNode reverseList(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+
+        while (head != null) {
+            stack.push(head);
+            head = head.next;
+        }
+
+        ListNode root = stack.isEmpty() ? null : stack.pop();
+        ListNode temp = root;
+        while (stack.size() > 0) {
+            temp.next = stack.pop();
+            temp = temp.next;
+        }
+        temp.next = null;
+        return root;
+    }
+}
