4c95b744222b8915848aa6d0b0fa783a5afacb48
lintq <smltq@126.com>
2020-03-04 17:53:12 +0800

    leetcode 第 543 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub543.java
@@ -0,0 +1,54 @@
+package com.easy.leetcode;
+
+/*
+
+给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过根结点。
+
+示例 :
+给定二叉树
+
+          1
+         / \
+        2   3
+       / \
+      4   5
+返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+
+注意：两结点之间的路径长度是以它们之间边的数目表示。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/diameter-of-binary-tree
+
+ */
+public class Sub543 {
+    public static void main(String[] args) {
+        Solution_543 solution = new Solution_543();
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println(solution.diameterOfBinaryTree(root));
+    }
+}
+
+class Solution_543 {
+    private int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return max;
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = depth(root.left);
+        int rightDepth = depth(root.right);
+        max = Math.max(leftDepth + rightDepth, max);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
