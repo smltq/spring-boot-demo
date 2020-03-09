1aa950022180c02f009a3a86be95aa4969b54329
lintq <smltq@126.com>
2020-03-09 15:30:11 +0800

    leetcode 第 101 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub101.java
@@ -0,0 +1,47 @@
+package com.easy.leetcode;
+
+/*
+101. 对称二叉树
+给定一个二叉树，检查它是否是镜像对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+说明:
+
+如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+ */
+public class Sub101 {
+    public static void main(String[] args) {
+        Solution_101 solution = new Solution_101();
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        System.out.println("是否镜像对称：" + solution.isSymmetric(root));
+    }
+}
+
+class Solution_101 {
+    public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+}
