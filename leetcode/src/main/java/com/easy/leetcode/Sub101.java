71427cac4434e8e065170380d295cf3605704fca
lintq <smltq@126.com>
2020-03-09 17:13:16 +0800

    leetcode 第 101 题

--- leetcode/src/main/java/com/easy/leetcode/Sub101.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub101.java
@@ -1,5 +1,8 @@
 package com.easy.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
 101. 对称二叉树
 给定一个二叉树，检查它是否是镜像对称的。
@@ -25,7 +28,7 @@ package com.easy.leetcode;
  */
 public class Sub101 {
     public static void main(String[] args) {
-        Solution_101 solution = new Solution_101();
+        Solution_101_2 solution = new Solution_101_2();
 
         TreeNode root = new TreeNode(1);
 
@@ -41,7 +44,10 @@ public class Sub101 {
     }
 }
 
-class Solution_101 {
+/**
+ * 递归 实现
+ */
+class Solution_101_1 {
     public boolean isSymmetric(TreeNode root) {
         return isMirror(root, root);
     }
@@ -54,5 +60,35 @@ class Solution_101 {
         } else {
             return (root1.val == root2.val) && isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left);
         }
+    }
+}
+
+/**
+ * 迭代 实现
+ */
+class Solution_101_2 {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            if (t1 == null && t2 == null) {
+                continue;
+            } else if (t1 == null || t2 == null) {
+                return false;
+            } else {
+                if (t1.val == t2.val) {
+                    queue.add(t1.left);
+                    queue.add(t2.right);
+                    queue.add(t1.right);
+                    queue.add(t2.left);
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
