8c5b480b65cc890e9d92f92839fcc7f7ea6446e4
lintq <smltq@126.com>
2020-03-12 11:51:20 +0800

    docker示例

---
+++ docker/src/main/java/com/easy/docker/DockerApplication.java
@@ -0,0 +1,12 @@
+package com.easy.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+}
