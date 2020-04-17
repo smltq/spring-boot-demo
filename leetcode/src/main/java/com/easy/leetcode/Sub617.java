5587f16671721681a5e7dd064619fd515c72bdbd
lintq <smltq@126.com>
2020-04-17 11:34:10 +0800

    力扣第 617 题

--- leetcode/src/main/java/com/easy/leetcode/Sub617.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub617.java
@@ -68,6 +68,16 @@ public class Sub617 {
  */
 class Solution_617 {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        return null;
+        if (t1 == null) {
+            return t2;
+        }
+
+        if (t2 == null) {
+            return t1;
+        }
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
     }
 }
