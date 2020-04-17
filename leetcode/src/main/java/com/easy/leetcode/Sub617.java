d136296e11e884e01fd9fdc0e6ba9907d9c8830a
lintq <smltq@126.com>
2020-04-17 11:22:40 +0800

    力扣第 617 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub617.java
@@ -0,0 +1,73 @@
+package com.easy.leetcode;
+
+/*
+617. 合并二叉树
+
+给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+
+你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+
+示例 1:
+
+输入:
+	Tree 1                     Tree 2
+          1                         2
+         / \                       / \
+        3   2                     1   3
+       /                           \   \
+      5                             4   7
+输出:
+合并后的树:
+	     3
+	    / \
+	   4   5
+	  / \   \
+	 5   4   7
+注意: 合并必须从两个树的根节点开始。
+
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-two-binary-trees
+ */
+public class Sub617 {
+    public static void main(String[] args) {
+        Solution_617 solution = new Solution_617();
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(3);
+        root1.right = new TreeNode(2);
+        root1.left.left = new TreeNode(5);
+
+        TreeNode root2 = new TreeNode(2);
+        root2.left = new TreeNode(1);
+        root2.right = new TreeNode(3);
+        root2.left.right = new TreeNode(4);
+        root2.right.right = new TreeNode(7);
+        TreeNode root = solution.mergeTrees(root1, root2);
+        outTree(root);
+    }
+
+    //中序输出
+    static void outTree(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.val + "\t");
+            outTree(root.left);
+            outTree(root.right);
+        }
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution_617 {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        return null;
+    }
+}
