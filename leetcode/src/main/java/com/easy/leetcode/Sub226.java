117d15486e64f4244a6bc9f56b15a325c49c6292
lintq <smltq@126.com>
2020-04-21 14:47:55 +0800

    力扣第 226 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub226.java
@@ -0,0 +1,72 @@
+package com.easy.leetcode;
+
+/*
+
+226. 翻转二叉树
+
+翻转一棵二叉树。
+
+示例：
+
+输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+备注:
+这个问题是受到 Max Howell 的 原问题 启发的 ：
+
+谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/invert-binary-tree
+
+ */
+public class Sub226 {
+    public static void main(String[] args) {
+        Solution_226 solution = new Solution_226();
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        TreeNode result = solution.invertTree(root);
+        outTree(result);
+    }
+
+    //中序输出
+    static void outTree(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.val + "\t");
+            outTree(root.left);
+            outTree(root.right);
+        }
+    }
+}
+
+/**
+ * 递归
+ */
+class Solution_226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        root.left = invertTree(root.left);
+        root.right = invertTree(root.right);
+        return root;
+    }
+}
