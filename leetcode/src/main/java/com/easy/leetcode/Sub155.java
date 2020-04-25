b37145e828043bf2dda431795bf635e626d9dd11
lintq <smltq@126.com>
2020-04-25 17:02:24 +0800

    力扣第 125 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub155.java
@@ -0,0 +1,93 @@
+package com.easy.leetcode;
+
+import java.util.Stack;
+
+/*
+155. 最小栈
+
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+push(x) —— 将元素 x 推入栈中。
+pop() —— 删除栈顶的元素。
+top() —— 获取栈顶元素。
+getMin() —— 检索栈中的最小元素。
+
+
+示例:
+
+输入：
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+输出：
+[null,null,null,null,-3,null,0,-2]
+
+解释：
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+
+提示：
+
+pop、top 和 getMin 操作总是在 非空栈 上调用。
+ */
+public class Sub155 {
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println("getMin:" + minStack.getMin());  //-3
+        minStack.pop();
+        System.out.println("top:" + minStack.top());    //0
+        System.out.println("getMin:" + minStack.getMin());  //-2
+    }
+}
+
+class MinStack {
+    Stack<Integer> stack = new Stack<>();
+    int min = Integer.MAX_VALUE;
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        if (x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        if (stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
