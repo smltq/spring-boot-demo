a3645b10721c0343fa6abfa4791a1ebe3e864ab3
lintq <smltq@126.com>
2020-04-27 10:56:39 +0800

    力扣第 53 题

--- leetcode/src/main/java/com/easy/leetcode/Sub53.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub53.java
@@ -24,7 +24,14 @@ public class Sub53 {
 
 class Solution_53 {
     public int maxSubArray(int[] nums) {
-
-        return 0;
+        if (nums == null || nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], 0) + nums[i];
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
     }
 }
