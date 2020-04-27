bb92c5e9b5b521eead992159f6eda7daeaa819b5
lintq <smltq@126.com>
2020-04-27 10:26:32 +0800

    力扣第 53 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub53.java
@@ -0,0 +1,30 @@
+package com.easy.leetcode;
+
+/*
+53. 最大子序和
+给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+示例:
+
+输入: [-2,1,-3,4,-1,2,1,-5,4],
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+
+进阶:
+
+如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ */
+public class Sub53 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        Solution_53 solution = new Solution_53();
+        System.out.println("最大和为：" + solution.maxSubArray(nums));
+    }
+}
+
+class Solution_53 {
+    public int maxSubArray(int[] nums) {
+
+        return 0;
+    }
+}
