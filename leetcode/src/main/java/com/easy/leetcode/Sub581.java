eae331dd0c252fddd2bbaea8300bab09b80eea73
lintq <smltq@126.com>
2020-04-29 16:21:10 +0800

    力扣第 581 题

--- leetcode/src/main/java/com/easy/leetcode/Sub581.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub581.java
@@ -21,7 +21,7 @@ import java.util.Arrays;
 public class Sub581 {
     public static void main(String[] args) {
         int[] nums = new int[]{2, 6, 4, 8, 10, 9, 15};
-        Solution_581 solution = new Solution_581();
+        Solution_581_2 solution = new Solution_581_2();
         System.out.println("输出：" + solution.findUnsortedSubarray(nums));
     }
 }
@@ -29,7 +29,7 @@ public class Sub581 {
 /**
  * 排序法
  */
-class Solution_581 {
+class Solution_581_1 {
     public int findUnsortedSubarray(int[] nums) {
         int[] numsSort = nums.clone();
         Arrays.sort(numsSort);
@@ -41,5 +41,68 @@ class Solution_581 {
             }
         }
         return (e - s >= 0 ? e - s + 1 : 0);
+    }
+}
+
+/**
+ * 1.先统计乱序的起始和终止位置，并求出这段序列里的最大最小值
+ * 2.然后乱序列前面的递增序列从后往前与min比较
+ * 3.乱序列后面的递增序列从前往后与max比较
+ */
+class Solution_581_2 {
+    public int findUnsortedSubarray(int[] nums) {
+        if (nums == null || nums.length < 2) return 0;
+        int len = nums.length;
+        //乱序列里的最小值
+        int min = Integer.MAX_VALUE;
+        //乱序列里的最大值
+        int max = Integer.MIN_VALUE;
+        //乱序列的起始位置
+        int start = 0;
+        //乱序列的终止位置
+        int end = 0;
+
+        //找乱序的起始索引
+        for (int i = 0; i < len - 1; i++) {
+            if (nums[i + 1] < nums[i]) {
+                start = i;
+                break;
+            }
+        }
+
+        //找乱序的终止索引
+        for (int i = len - 1; i > 0; i--) {
+            if (nums[i - 1] > nums[i]) {
+                end = i;
+                break;
+            }
+        }
+
+        //找乱序中的最大值、最小值
+        for (int i = start; i <= end; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+        if (start == end) return 0;
+
+        //找开始索引,乱序列前面的递增序列从后往前与min比较
+        while (start > 0) {
+            if (nums[start - 1] > min) {
+                start--;
+            } else {
+                break;
+            }
+        }
+
+        //找结束索引,乱序列后面的递增序列从前往后与max比较
+        while (end < len - 1) {
+            if (nums[end + 1] < max) {
+                end++;
+            } else {
+                break;
+            }
+        }
+
+        return end - start + 1;
     }
 }
