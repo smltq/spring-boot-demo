6b1d5b140d42146f171132eb6af73008f9474d05
lintq <smltq@126.com>
2020-04-30 17:50:18 +0800

    力扣第 22 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub22.java
@@ -0,0 +1,33 @@
+package com.easy.leetcode;
+
+import java.util.List;
+
+/*
+22. 括号生成
+数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+
+
+
+示例：
+
+输入：n = 3
+输出：[
+       "((()))",
+       "(()())",
+       "(())()",
+       "()(())",
+       "()()()"
+     ]
+ */
+public class Sub22 {
+    public static void main(String[] args) {
+        Solution_22 solution = new Solution_22();
+        System.out.println("输出：" + solution.generateParenthesis(3));
+    }
+}
+
+class Solution_22 {
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
+}
