1bc0aa3f52185733e43aaa7611ce1327842c38c8
lintq <smltq@126.com>
2020-05-06 15:56:52 +0800

    力扣第 22 题

--- leetcode/src/main/java/com/easy/leetcode/Sub22.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub22.java
@@ -8,7 +8,6 @@ import java.util.List;
 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
 
 
-
 示例：
 
 输入：n = 3
@@ -27,30 +26,40 @@ public class Sub22 {
     }
 }
 
+/**
+ * 回溯法-递归实现
+ */
 class Solution_22 {
     List<String> result = new ArrayList<>();
 
     public List<String> generateParenthesis(int n) {
-        backtrack(n, n, new StringBuilder());
+        find(0, 0, n, new StringBuilder());
         return result;
     }
 
-    public void backtrack(int open, int close, StringBuilder path) {
-        if (close < open) return;
+    public void find(int open, int close, int n, StringBuilder path) {
+        //剪枝，开括号比闭括号少
+        if (open < close) return;
 
-        if (open < 0 || close < 0) return;
+        //剪枝，开括号或闭括号数量超过n
+        if (open > n || close > n) return;
 
-        if (open == 0 && close == 0) {
+        //开闭括号数量刚好等于n，满足条件，保存到结果列表
+        if (open == n && close == n) {
             result.add(path.toString());
             return;
         }
 
+        //添加开扣号
         path.append("(");
-        backtrack(open - 1, close, path);
+        find(open + 1, close, n, path);
+        //撤销开扣号
         path.deleteCharAt(path.length() - 1);
 
+        //添加右括号
         path.append(")");
-        backtrack(open, close - 1, path);
+        find(open, close + 1, n, path);
+        //撤销右括号
         path.deleteCharAt(path.length() - 1);
     }
 }
