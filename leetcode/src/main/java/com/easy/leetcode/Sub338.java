1f9fef8b854f7aa04a850524fb9d239f9fddfc88
lintq <smltq@126.com>
2020-05-06 16:11:19 +0800

    力扣第 338 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub338.java
@@ -0,0 +1,39 @@
+package com.easy.leetcode;
+
+/*
+338. 比特位计数
+给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
+
+示例 1:
+
+输入: 2
+输出: [0,1,1]
+示例 2:
+
+输入: 5
+输出: [0,1,1,2,1,2]
+进阶:
+
+给出时间复杂度为O(n*sizeof(integer))的解答非常容易。但你可以在线性时间O(n)内用一趟扫描做到吗？
+要求算法的空间复杂度为O(n)。
+你能进一步完善解法吗？要求在C++或任何其他语言中不使用任何内置函数（如 C++ 中的 __builtin_popcount）来执行此操作。
+ */
+public class Sub338 {
+    public static void main(String[] args) {
+        Solution_338 solution = new Solution_338();
+        System.out.println("输出：" + solution.countBits(2));
+    }
+}
+
+/**
+ * 使用内置位统计函数
+ */
+class Solution_338 {
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+            ans[i] = Integer.bitCount(i);
+        }
+        return ans;
+    }
+}
