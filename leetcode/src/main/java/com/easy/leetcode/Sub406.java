278d6bafe8e4e19b0f19910e5740c41918ec28da
lintq <smltq@126.com>
2020-05-29 17:44:54 +0800

    力扣第 406 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub406.java
@@ -0,0 +1,43 @@
+package com.easy.leetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+
+406. 根据身高重建队列
+假设有打乱顺序的一群人站成一个队列。 每个人由一个整数对(h, k)表示，其中h是这个人的身高，k是排在这个人前面且身高大于或等于h的人数。 编写一个算法来重建这个队列。
+
+注意：
+总人数少于1100人。
+
+示例
+
+输入:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+输出:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+ */
+public class Sub406 {
+    public static void main(String[] args) {
+        int[][] people = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
+        Solution_406 solution = new Solution_406();
+        int[][] ans = solution.reconstructQueue(people);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(Arrays.toString(ans[i]));
+        }
+    }
+}
+
+class Solution_406 {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0];
+            }
+        });
+        return people;
+    }
+}
