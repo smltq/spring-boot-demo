65eff6a1764494ec24478415669b74a9940d5f73
lintq <smltq@126.com>
2020-06-17 13:29:02 +0800

    力扣第 739 题

--- leetcode/src/main/java/com/easy/leetcode/Sub739.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub739.java
@@ -1,5 +1,7 @@
 package com.easy.leetcode;
 
+import java.util.Arrays;
+
 /*
 739. 每日温度
 根据每日 气温 列表，请重新生成一个列表，对应位置的输出是需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。
@@ -12,14 +14,25 @@ public class Sub739 {
     public static void main(String[] args) {
         int[] T = {73, 74, 75, 71, 69, 72, 76, 73};
         Solution_739_1 solution = new Solution_739_1();
-        System.out.println("返回结果为：" + solution.dailyTemperatures(T));
+        System.out.println("返回结果为：" + Arrays.toString(solution.dailyTemperatures(T)));
     }
 }
 
+/**
+ * 暴力解法
+ */
 class Solution_739_1 {
     public int[] dailyTemperatures(int[] T) {
-        int[] ans = new int[T.length];
-
+        int len = T.length;
+        int[] ans = new int[len];
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (T[j] > T[i]) {
+                    ans[i] = j - i;
+                    break;
+                }
+            }
+        }
         return ans;
     }
 }
