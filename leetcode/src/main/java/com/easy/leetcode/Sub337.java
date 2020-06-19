fb134d5c1827fe1fedb29eae8f9c3a2b55d39fb9
lintq <smltq@126.com>
2020-06-19 10:03:34 +0800

    力扣第 337 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub337.java
@@ -0,0 +1,47 @@
+package com.easy.leetcode;
+
+/*
+337. 打家劫舍 III
+在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+
+计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+
+示例 1:
+
+输入: [3,2,3,null,3,null,1]
+
+     3
+    / \
+   2   3
+    \   \
+     3   1
+
+输出: 7
+解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7.
+示例 2:
+
+输入: [3,4,5,1,3,null,1]
+
+     3
+    / \
+   4   5
+  / \   \
+ 1   3   1
+
+输出: 9
+解释: 小偷一晚能够盗取的最高金额 = 4 + 5 = 9.
+ */
+
+public class Sub337 {
+    public static void main(String[] args) {
+        Solution_337 solution = new Solution_337();
+        TreeNode root = new TreeNode(3);
+        System.out.println("输出：" + solution.rob(root));
+    }
+}
+
+class Solution_337 {
+    public int rob(TreeNode root) {
+        return 0;
+    }
+}
