b13942bbd9d923f70d03846f5e77b978c9d94f4e
lintq <smltq@126.com>
2020-06-29 16:51:09 +0800

    力扣第 339 题

--- leetcode/src/main/java/com/easy/leetcode/Sub339.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub339.java
@@ -1,6 +1,6 @@
 package com.easy.leetcode;
 
-import java.util.List;
+import java.util.*;
 
 /*
 399. 除法求值
@@ -22,9 +22,49 @@ queries(问题方程式) = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x
  */
 public class Sub339 {
     public static void main(String[] args) {
-        int[] nums = {2, 1};
+        List<List<String>> equations = new ArrayList<>();
+
+        List<String> e1 = new ArrayList<>();
+        e1.add("a");
+        e1.add("b");
+        equations.add(e1);
+
+        List<String> e2 = new ArrayList<>();
+        e2.add("b");
+        e2.add("c");
+        equations.add(e2);
+
+        double[] values = {2.0, 3.0};
+
+        List<List<String>> queries = new ArrayList<>();
+
+        List<String> q1 = new ArrayList<>();
+        q1.add("a");
+        q1.add("c");
+        queries.add(q1);
+
+        List<String> q2 = new ArrayList<>();
+        q2.add("b");
+        q2.add("a");
+        queries.add(q2);
+
+        List<String> q3 = new ArrayList<>();
+        q3.add("a");
+        q3.add("e");
+        queries.add(q3);
+
+        List<String> q4 = new ArrayList<>();
+        q4.add("a");
+        q4.add("a");
+        queries.add(q4);
+
+        List<String> q5 = new ArrayList<>();
+        q5.add("x");
+        q5.add("x");
+        queries.add(q5);
+
         Solution_339 solution = new Solution_339();
-        System.out.println("返回结果为：");
+        System.out.println("返回结果为：" + Arrays.toString(solution.calcEquation(equations, values, queries)));
     }
 }
 
@@ -32,7 +72,79 @@ public class Sub339 {
  * 三指针解法
  */
 class Solution_339 {
+    //child parent
+    Map<String, String> parents = new HashMap<>();
+    //child mutiply of parent
+    Map<String, Double> values = new HashMap<>();
+
+    public void add(String x) {
+        if (!parents.containsKey(x)) {
+            parents.put(x, x);
+            values.put(x, 1.0);
+        }
+    }
+
+    public void union(String parent, String child, double value) {
+
+        add(parent);
+        add(child);
+        String p1 = find(parent);
+        String p2 = find(child);
+        if (p1 == p2) {
+            return;
+        } else {
+            parents.put(p2, p1);
+            values.put(p2, value * (values.get(parent) / values.get(child)));
+        }
+    }
+
+    public String find(String x) {
+        while (parents.get(x) != x) {
+            x = parents.get(x);
+        }
+        return x;
+    }
+
+    public double cal(String x) {
+        // System.out.println("cal x"+x);
+        double v = values.get(x);
+        while (parents.get(x) != x) {
+            x = parents.get(x);
+            v *= values.get(x);
+        }
+
+        return v;
+    }
+
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        return null;
+
+        //union
+        for (int i = 0; i < equations.size(); i++) {
+            //union parent child value
+            union(equations.get(i).get(0), equations.get(i).get(1), values[i]);
+        }
+
+        //find
+        double[] ans = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+            String c1 = queries.get(i).get(0);
+            String c2 = queries.get(i).get(1);
+            if (!parents.containsKey(c1) || !parents.containsKey(c2)) {
+                ans[i] = -1;
+                continue;
+            }
+            if (c1.equals(c2)) {
+                ans[i] = 1;
+                continue;
+            }
+            String p1 = find(c1);
+            String p2 = find(c2);
+            if (!p1.equals(p2)) {
+                ans[i] = -1;
+                continue;
+            }
+            ans[i] = cal(c2) / cal(c1);
+        }
+        return ans;
     }
 }
