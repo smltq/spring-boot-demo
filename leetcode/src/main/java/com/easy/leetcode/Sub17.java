10fa37a0e7e3d87a7cbe1d83df3d01b9e54187ed
lintq <smltq@126.com>
2020-07-17 17:12:36 +0800

    力扣第 17 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub17.java
@@ -0,0 +1,31 @@
+package com.easy.leetcode;
+
+import java.util.List;
+
+/*
+17. 电话号码的字母组合
+给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+
+
+示例:
+
+输入："23"
+输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+说明:
+尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+ */
+public class Sub17 {
+    public static void main(String[] args) {
+        Solution_17 solution = new Solution_17();
+        System.out.println("返回结果为：" + solution.letterCombinations("23"));
+    }
+}
+
+class Solution_17 {
+    public List<String> letterCombinations(String digits) {
+        return null;
+    }
+}
