4d4b1076227ac0d05e79a67171651aea4a32d463
lintq <smltq@126.com>
2020-07-17 17:48:13 +0800

    力扣第 17 题

--- leetcode/src/main/java/com/easy/leetcode/Sub17.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub17.java
@@ -1,6 +1,9 @@
 package com.easy.leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
 17. 电话号码的字母组合
@@ -25,7 +28,22 @@ public class Sub17 {
 }
 
 class Solution_17 {
+    Map<Character, String> map = new HashMap<Character, String>() {{
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+    }};
+
     public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 0; i < digits.length(); i++) {
+
+        }
         return null;
     }
 }
