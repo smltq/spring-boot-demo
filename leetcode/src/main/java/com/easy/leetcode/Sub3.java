b39a22d05ad6c9789981a508b663d28c21b5fc02
lintq <smltq@126.com>
2020-08-05 16:12:40 +0800

    力扣第 3 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub3.java
@@ -0,0 +1,33 @@
+package com.easy.leetcode;
+
+/*
+03. 数组中重复的数字
+找出数组中重复的数字。
+
+
+在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+
+示例 1：
+
+输入：
+[2, 3, 1, 0, 2, 5, 3]
+输出：2 或 3
+
+
+限制：
+
+2 <= n <= 100000
+*/
+public class Sub3 {
+    public static void main(String[] args) {
+        Solution_3 solution = new Solution_3();
+        int[] nums = new int[]{2, 3, 1, 0, 2, 5, 3};
+        System.out.println("返回结果为：" + solution.findRepeatNumber(nums));
+    }
+}
+
+class Solution_3 {
+    public int findRepeatNumber(int[] nums) {
+        return 0;
+    }
+}
