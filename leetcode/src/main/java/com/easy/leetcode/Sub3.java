f3434242d9632c348473a83a44fea11f1c3fc84a
lintq <smltq@126.com>
2020-08-06 11:46:46 +0800

    力扣第 3 题

--- leetcode/src/main/java/com/easy/leetcode/Sub3.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub3.java
@@ -1,5 +1,8 @@
 package com.easy.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 3. 无重复字符的最长子串
 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -24,14 +27,30 @@ package com.easy.leetcode;
 public class Sub3 {
     public static void main(String[] args) {
         Solution_3 solution = new Solution_3();
-        String s = "abcabcbb";
+        String s = "pwwkew";
         int ans = solution.lengthOfLongestSubstring(s);
         System.out.println("结果：" + ans);
     }
 }
 
+/**
+ * 利用 双指针+哈希 实现
+ */
 class Solution_3 {
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        char[] chars = s.toCharArray();
+        int len = chars.length, ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0, j = 0; i < len; i++) {
+            if (map.containsKey(chars[i])) {
+                int t = map.get(chars[i]);
+                while (j <= t) {
+                    map.remove(chars[j++]);
+                }
+            }
+            map.put(chars[i], i);
+            ans = Math.max(ans, map.size());
+        }
+        return ans;
     }
 }
