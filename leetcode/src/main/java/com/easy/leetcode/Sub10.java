170e6fcab57eb81788ae4bb211c3536ea0d5ab29
ltq <smltq@126.com>
2020-08-21 10:21:25 +0800

    力扣第 10 题

--- leetcode/src/main/java/com/easy/leetcode/Sub10.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub10.java
@@ -50,14 +50,42 @@ p = "mis*is*p*."
 public class Sub10 {
     public static void main(String[] args) {
         Solution_10 solution = new Solution_10();
-        String s = "";
-        String p = "";
+        String s = "aab";
+        String p = "c*a*b";
         System.out.println("返回结果为：" + solution.isMatch(s, p));
     }
 }
 
 class Solution_10 {
     public boolean isMatch(String s, String p) {
-        return true;
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                }
+
+            }
+        }
+        return dp[m][n];
+    }
+
+    public boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
     }
 }
