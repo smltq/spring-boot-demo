2d042283d841092e5f1b3b48e771d8501fc20fbf
lintq <smltq@126.com>
2020-10-21 10:38:57 +0800

    力扣第 55 题

---
+++ leetcode/src/main/java/com/easy/leetcode/Sub56.java
@@ -0,0 +1,34 @@
+package com.easy.leetcode;
+
+/*
+56. 合并区间
+给出一个区间的集合，请合并所有重叠的区间。
+
+示例 1:
+
+输入: intervals = [[1,3],[2,6],[8,10],[15,18]]
+输出: [[1,6],[8,10],[15,18]]
+解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+示例 2:
+
+输入: intervals = [[1,4],[4,5]]
+输出: [[1,5]]
+解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+
+提示：
+
+intervals[i][0] <= intervals[i][1]
+ */
+public class Sub56 {
+    public static void main(String[] args) {
+        Solution_42_1 solution = new Solution_42_1();
+        int[] nums = {};
+        System.out.println("返回结果为：" + solution.trap(nums));
+    }
+}
+
+class Solution_56 {
+    public int[][] merge(int[][] intervals) {
+        return null;
+    }
+}
