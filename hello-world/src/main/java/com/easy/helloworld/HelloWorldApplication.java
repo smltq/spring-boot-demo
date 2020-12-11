0e382309d792ed158f1bc7add1d53a752d6023da
lintq <smltq@126.com>
2020-12-11 09:57:57 +0800

    版本升级、命名规划调整

--- hello-world/src/main/java/com/easy/helloworld/HelloWorldApplication.java
+++ hello-world/src/main/java/com/easy/helloworld/HelloWorldApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloworldApplication {
-
+public class HelloWorldApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HelloworldApplication.class, args);
+        SpringApplication.run(HelloWorldApplication.class, args);
     }
 }
