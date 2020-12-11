6c789de5176f1cc832de62b30314c3922f8a21cd
lintq <smltq@126.com>
2020-12-11 10:50:25 +0800

    升级为spring boot 2.4.0

--- data-redis/src/test/java/demo.data.redis/TestRedis1.java
+++ data-redis/src/test/java/demo.data.redis/TestRedis1.java
@@ -2,17 +2,17 @@ package demo.data.redis;
 
 import demo.data.redis.model.User;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Slf4j
 public class TestRedis1 {
@@ -27,7 +27,7 @@ public class TestRedis1 {
     public void test() throws Exception {
         stringRedisTemplate.opsForValue().set("aaa", "111");
         log.info(String.format("aaa值是：%s", stringRedisTemplate.opsForValue().get("aaa")));
-        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+        Assertions.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
     }
 
     @Test
--- data-redis/src/test/java/demo.data.redis/TestRedis2.java
+++ data-redis/src/test/java/demo.data.redis/TestRedis2.java
@@ -1,15 +1,14 @@
 package demo.data.redis;
-
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Slf4j
 public class TestRedis2 {
