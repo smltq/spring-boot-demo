2ff460017dcbcfc5dd819b2c00e123dbb91c2199
lintq <smltq@126.com>
2022-08-04 11:15:59 +0800

    力扣第 78 题

--- leetcode/src/main/java/com/easy/leetcode/Sub78.java
+++ leetcode/src/main/java/com/easy/leetcode/Sub78.java
@@ -26,13 +26,13 @@ import java.util.List;
  */
 public class Sub78 {
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3};
-        Solution_78 solution = new Solution_78();
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Solution_78_2 solution = new Solution_78_2();
         System.out.println("输出：" + solution.subsets(nums));
     }
 }
 
-class Solution_78 {
+class Solution_78_1 {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> output = new ArrayList();
         output.add(new ArrayList<>());
@@ -49,5 +49,26 @@ class Solution_78 {
             }
         }
         return output;
+    }
+}
+
+class Solution_78_2 {
+    List<Integer> t = new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(0, nums);
+        return ans;
+    }
+
+    public void dfs(int cur, int[] nums) {
+        if (cur == nums.length) {
+            ans.add(new ArrayList<>(t));
+            return;
+        }
+        t.add(nums[cur]);
+        dfs(cur + 1, nums);
+        t.remove(t.size() - 1);
+        dfs(cur + 1, nums);
     }
 }
