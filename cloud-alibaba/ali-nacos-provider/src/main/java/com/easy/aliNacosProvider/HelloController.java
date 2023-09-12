8839a2bf004e41c45a869a19537c81293f4292ed
lintingqian <271657370@qq.com>
2023-09-12 16:08:33 +0800

    Spring Cloud Alibaba 和 Spring Cloud 版本调整

--- cloud-alibaba/ali-nacos-provider/src/main/java/com/easy/aliNacosProvider/HelloController.java
+++ cloud-alibaba/ali-nacos-provider/src/main/java/com/easy/aliNacosProvider/HelloController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping(value = "/hello/{str}", produces = "application/json")
     public String hello(@PathVariable String str) {
-        log.info("-----------收到消费者请求-----------");
-        log.info("收到消费者传递的参数：" + str);
+        //log.info("-----------收到消费者请求-----------");
+        //log.info("收到消费者传递的参数：" + str);
         String result = "我是服务提供者，见到你很高兴==>" + str;
-        log.info("提供者返回结果：" + result);
+        //log.info("提供者返回结果：" + result);
         return result;
     }
 }
